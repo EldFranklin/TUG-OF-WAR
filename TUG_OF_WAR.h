8b7ab8a99679e8eda18312074496b30db0d0a4b5
EldFranklin <105466304+EldFranklin@users.noreply.github.com>
2023-12-07 15:44:06 -0300

    Update TUG_OF_WAR.h
    Makefile funcionando

--- TUG_OF_WAR.h
+++ TUG_OF_WAR.h
@@ -1,5 +1,5 @@
-#ifndef TUF_OF_WAR_H
-#define TUF_OF_WAR_H
+#ifndef TUG_OF_WAR_H
+#define TUG_OF_WAR_H
 
 
 //LEDS EMBARCADOS
@@ -11,6 +11,16 @@ void usr_leds(int ledn, int value);
 void configure_gpio_output(int gpio_number);
 void configure_gpio_input(int gpio_number);
 void set_gpio_value(int gpio_number, int value);
+char read_gpio(int gpio_number);
 
 
-#endif //TUF_OF_WAR_H
+void export_gpio_PWM(int gpio_number);
+void configurar_pino_saida_PWM(int gpio_number);
+void gerar_pwm(int gpio_number, int frequencia, float dutycicle);
+void read_rtc0_time();
+void all_leds(int x);
+void PWM0(int pwm_number,int period,int duty_cycle);
+void on_pwm0(int status);
+void debounce_delay();
+
+#endif //TUG_OF_WAR_H
